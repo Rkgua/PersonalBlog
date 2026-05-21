fca88b1010bbab30e90217807fe9f57cdf652c82
Rkgua <3135450840@qq.com>
2026-05-21 15:29:52 +0800

    feat: 添加 terser 作为构建工具，支持代码压缩和去除控制台日志

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,18 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+      "/uploads": "http://localhost:5000",
+    },
+  },
 });
